9ac5d9c13a038f36cd8f09313aec1c9f11925e54
kd2156-coder <kd2156@srmist.edu.in>
2026-02-24 11:23:40 +0530

    coded to print oops banner using arry-init

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,16 +1,19 @@
-/**
- * OOPSBannerApp UC1 - OOPS Banner Display Application
- *
- * This class demonstrates a simple Java application that displays the Object
- * Oriented Programming System OOPS acronym to the console.
- *
- * @author Developer
- * @version 1.0
- */
-
 public class OOPSBannerApp {
 
     public static void main(String[] args) {
-        System.out.println("OOPS");
+
+        // Inline array initialization using String.join()
+        String[] lines = {
+            String.join(" ", "*****", "*****", "*****", "*****"),
+            String.join(" ", "*   *", "*   *", "*   *", "*    "),
+            String.join(" ", "*   *", "*   *", "*****", "*****"),
+            String.join(" ", "*   *", "*   *", "*    ", "    *"),
+            String.join(" ", "*****", "*****", "*    ", "*****")
+        };
+
+        // Print banner
+        for (String line : lines) {
+            System.out.println(line);
+        }
     }
 }
